e80f99133798cb68428ddd85642e8f01b9e32a64
Angas <angasreid@gmail.com>
2014-10-09 17:47:56 -0400

    Updates to Index page

--- app/controllers/ditties_controller.rb
+++ app/controllers/ditties_controller.rb
@@ -1,5 +1,5 @@
 class DittiesController < ApplicationController
-    before_action :set_ditty, only: [:show, :edit, :update, :destroy]
+    before_action :set_ditty, only: [:edit, :update, :destroy]
   def index
     @ditties = Ditty.all
   end
